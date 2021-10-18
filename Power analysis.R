d3a38677229d3cf46283763ec7afdacc902ffa4c
Taemo Bang <favorite@kakao.com>
2021-10-18 17:54:16 +0900

    Initial commit

---
+++ Power analysis.R
@@ -0,0 +1,29 @@
+library(pwr)
+pwr.r.test(n = 22, r = -.295, sig.level = 0.05)
+pwr.r.test(power = 0.8, r = -.295, sig.level = 0.05)
+
+
+## 표 1
+pwr.r.test(n = 22, r = -.295, sig.level = 0.05)
+pwr.r.test(n = 22, r = -0.079, sig.level = 0.05)
+pwr.r.test(n = 22, r = -0.076, sig.level = 0.05)
+pwr.r.test(n = 22, r = -0.093, sig.level = 0.05)
+pwr.r.test(n = 22, r = -0.002, sig.level = 0.05)
+pwr.r.test(n = 22, r = -0.494, sig.level = 0.05)
+pwr.r.test(n = 22, r = 0.257, sig.level = 0.05)
+pwr.r.test(n = 22, r = -0.269, sig.level = 0.05)
+pwr.r.test(n = 22, r = -0.871, sig.level = 0.05)
+
+## 표 2
+pwr.r.test(n = 19, r = -0.258, sig.level = 0.05)
+
+## 표 3
+pwr.r.test(n = 19, r = -0.273, sig.level = 0.05)
+
+## 표 4
+pwr.r.test(n = 22, r = -0.216, sig.level = 0.05)
+pwr.r.test(n = 22, r = -0.298, sig.level = 0.05)
+
+## 표 5
+pwr.r.test(n = 22, r = 0.279, sig.level = 0.05)
+pwr.r.test(n = 22, r = 0.383, sig.level = 0.05)
